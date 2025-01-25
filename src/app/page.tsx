fa0c34704c266566678273c14fc574d39f8ea69d
kdw <50583568+dong336@users.noreply.github.com>
2025-01-25 22:40:15 +0900

    shadcn 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-blue-500">Welcome to the course !</h1>
+      <h1 className="text-blue-500 font-barlow">Welcome to the course !</h1>
+      <Button variant="destructive">Click here</Button>
     </div>
   );
 }
